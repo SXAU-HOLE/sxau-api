7863f9297a3ab893a5708d705f9b237115a9d778
彬腾 <2393714045@qq.com>
2023-11-17 20:34:02 +0800

    fix: 课表选择周次

--- src/modules/mobile/schedule.ts
+++ src/modules/mobile/schedule.ts
@@ -1,8 +1,10 @@
+import { Request, Response } from "express"
 import axios from "axios"
 
 export default async function (req: Request, res: Response) {
-  const url =
-    "http://jwglxtyd.sxau.edu.cn:8080/njwhd/student/curriculum?week=&kbjcmsid="
+  const query = req.query
+
+  const url = `http://jwglxtyd.sxau.edu.cn:8080/njwhd/student/curriculum?week=${query.week}`
 
   const { data } = await axios.get(url, {
     headers: {
