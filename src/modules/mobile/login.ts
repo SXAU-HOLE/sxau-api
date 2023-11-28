317c4d19415de64558dd50cecbb578ed348df817
彬腾 <2393714045@qq.com>
2023-11-28 20:37:40 +0800

    feat: 抽离路由逻辑

--- src/modules/mobile/login.ts
+++ src/modules/mobile/login.ts
@@ -3,15 +3,17 @@ import axios from "axios"
 import { encrypt } from "../../utils/encrypt"
 
 export default async function (req: Request, res: Response) {
-  const username = req.query.username
-  const password = req.query.password as string
+  const username = req.query?.username
+  const password = req.query?.password as string
+
+  console.log(req.query)
 
   const pwd = encrypt(password)
 
   const url = `http://jwglxtyd.sxau.edu.cn:8080/njwhd/login?userNo=${username}&pwd=${pwd}&encode=1&captchaData=&codeVal=`
 
   try {
-    const { data } = await axios.post(url)
+    const { data } = await axios.post(url, {})
     return data
   } catch (err) {
     throw new Error("请联系管理员，服务器出错啦~")
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,5 @@
-import { Express } from "express"
+import { Express, Request, Response, query } from "express"
 import * as fs from "fs"
-import login from "./modules/login"
-import MobileLogin from "./modules/mobile/login"
-import MobileSchedule from "./modules/mobile/schedule"
 
 async function getModule(mobile = false) {
   let files: string[] = []
@@ -17,27 +14,49 @@ async function getModule(mobile = false) {
     .filter((item) => item.endsWith(".ts"))
     .map((file) => {
       const fileName = file.replace(".ts", "")
+      let route = "/" + fileName
 
+      let get = false
+      if (fileName.includes("get")) {
+        get = true
+
+        route = route.replace("get_", "")
+      }
+
+      if (mobile) {
+        route = "/mobile" + route
+      }
+
+      // eslint-disable-next-line @typescript-eslint/no-var-requires
       const module = require(`./modules/${
         mobile ? "mobile/" : ""
       }${fileName}`).default
-      console.log(fileName, module)
 
-      return { module }
+      return { module, get, route }
     })
 }
 
+async function routerHandler(req: Request, res: Response, item: any) {
+  const moduleResponse = await item.module(req, res)
+  res.send(moduleResponse)
+}
+
 async function setupRoute(app: Express) {
-  const routes = [await getModule(), await getModule(true)]
-  // console.log(routes);
-  app.post("/login", async (req, res) => {
-    res.send(await login(req, res))
+  const routes = [...(await getModule()), ...(await getModule(true))]
+  console.log(routes)
+
+  routes.forEach((item) => {
+    console.log(item.module)
+
+    if (item.get) {
+      app.get(item.route, (req, res) => routerHandler(req, res, item))
+    } else {
+      app.post(item.route, (req, res) => routerHandler(req, res, item))
+    }
   })
-  app.post("/mobile/login", async (req, res) => {
-    res.send(await MobileLogin(req, res))
-  })
-  app.get("/mobile/schedule", async (req, res) => {
-    res.send(await MobileSchedule(req, res))
+
+  app.get("/test", (req, res) => {
+    res.send("test")
   })
 }
 
